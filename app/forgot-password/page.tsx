da0f32ff1e0299747dc7d0ec53b199b3259e4045
Syntax365 <synxtax365@gmail.com>
2024-08-10 10:24:12 -0400

    authz

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -1,5 +1,6 @@
 'use client';
-import SignUpForm from '@/components/sign-up-form';
+
+import ForgotPasswordForm from '@/components/forgot-password-form';
 
 export default function SignUp() {
   return (
@@ -11,13 +12,17 @@ export default function SignUp() {
             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
             alt="Your Company"
           />
-          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Create your account
+          <h2 className="mb-3 mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Trouble Logging In?
           </h2>
+          <p className="text-center">
+            Enter your email address and we'll send you a link to get back into
+            your account.
+          </p>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
-          <div className="bg-white px-6 py-10 shadow sm:rounded-lg sm:px-12">
+          <div className=" bg-white px-6 py-8 shadow sm:rounded-lg sm:px-12">
             <button
               onClick={() => {
                 if (window) {
@@ -26,7 +31,7 @@ export default function SignUp() {
               }}
               className="flex flex-row pb-4 text-indigo-600 hover:text-indigo-500"
             >
-              <svg
+              <svg 
                 xmlns="http://www.w3.org/2000/svg"
                 height="24"
                 viewBox="0 -960 960 960"
@@ -39,7 +44,7 @@ export default function SignUp() {
                 Back
               </span>
             </button>
-            <SignUpForm />
+            <ForgotPasswordForm />
           </div>
         </div>
       </div>
